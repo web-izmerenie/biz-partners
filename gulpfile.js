c5adf7f872d450661d78d394b1171a0fc806aa8a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-10-19 15:40:51 +0300

    fixed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ var connect = require('gulp-connect');
 var browserify = require('gulp-browserify');
 
 gulp.task('styles-clean', function () {
-	return gulp.src('./styles/build/*', {read: false})
-	.pipe(clean());
+	return gulp.src('./styles/build/*', { read: false })
+		.pipe(clean());
 });
 
 gulp.task('styles', ['styles-clean'], function () {
@@ -21,13 +21,13 @@ gulp.task('styles', ['styles-clean'], function () {
 		.pipe(gulp.dest('./styles/build/'));
 });
 
-gulp.task('jade', function(){
+gulp.task('jade', function () {
 	gulp.src('./jade/*.jade')
 		.pipe(jade({pretty: true}))
 		.pipe(gulp.dest('./html/'));
 });
 
-gulp.task('coffee', function() {
+gulp.task('coffee', function () {
 	gulp.src('./scripts/src/main.coffee', { read: false })
 		.pipe(browserify({
 			transform: ['coffeeify'],
@@ -38,14 +38,14 @@ gulp.task('coffee', function() {
 		.pipe(gulp.dest('./scripts/build/'))
 });
 
-gulp.task('connect', function() {
-  connect.server();
+gulp.task('connect', function () {
+	connect.server();
 });
 
-gulp.task('watch', ['jade','styles', 'coffee'], function(){
+gulp.task('watch', ['jade', 'styles', 'coffee'], function () {
 	gulp.watch('./jade/**/*.jade', ['jade']);
 	gulp.watch('./styles/src/**/*.less', ['styles']);
 	gulp.watch('./scripts/src/**/*.coffee', ['coffee']);
 });
 
-gulp.task('default', ['connect', 'jade','styles', 'coffee']);
+gulp.task('default', ['connect', 'jade', 'styles', 'coffee']);
