0f88e8b23dc37ef271ad484bbf9b10955c7cedbb
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-10-28 16:36:14 +0300

    Init bitrix template

--- bitrix/templates/main/footer.php
+++ bitrix/templates/main/footer.php
@@ -2,5 +2,95 @@
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
 ?>
-	</body>
+</main>
+<footer>
+	<div class="wrapper">
+		<div id="copy" class="column">© Biz Partners, 2001-2015
+			<p><b>Адрес: 107023 г. Москва,</b> <br>
+				ул. Большая почтовая, 36, <br>
+				18 подъезд, сектор А, 1 этаж, офис 110
+
+			</p>
+		</div>
+		<div id="footer-menu" class="column">
+			<ul>
+				<li><a href="#">О компании</a></li>
+				<li><a href="#">Каталог услуг</a></li>
+				<li><a href="#">Новости</a></li>
+				<li><a href="#">Контакты</a></li>
+			</ul>
+			<ul>
+				<li>Каталог услуг</li>
+				<li><a href="#">Бухгалтерский и налоговыйучет</a></li>
+				<li><a href="#">Корпоративные юридические услуги</a></li>
+				<li><a href="#">Кадровое законодательство</a></li>
+				<li><a href="#">Аудит</a></li>
+				<li><a href="#">Дополнительные услуги</a></li>
+				<li><a href="#">Подбор персонала</a></li>
+				<li><a href="#">Услуги для физических лиц</a></li>
+			</ul>
+		</div>
+		<div id="developer" class="column"><a href="http://web-izmerenie.ru/">сделано в</a></div>
+	</div>
+</footer>
+<div class="overlay"></div>
+<div id="callme" class="module-window">
+	<div class="title"><img src="../bitrix/templates/main/img/green-call.png">
+		<p>Заказать звонок</p>
+	</div>
+	<form method="post">
+		<input type="text" name="name" placeholder="Имя" class="require">
+		<input type="text" name="name" placeholder="Телефон" class="require">
+		<input type="text" name="name" placeholder="Удобное для вас время" class="require">
+		<div class="error">Ошибка отправки. Необходимо заполнить все поля</div>
+		<input type="submit" value="Отправить">
+		<div class="sucsess">
+			<p>«Спасибо за заявку.<br>
+				Мы свяжемся с вами
+				в течение часа».
+
+			</p>
+		</div>
+	</form>
+</div>
+<div id="writeme" class="module-window">
+	<div class="title"><img src="../bitrix/templates/main/img/green-mail.png">
+		<p>Написать нам</p>
+	</div>
+	<form method="post">
+		<input type="text" name="name" placeholder="Имя" class="require">
+		<input type="text" name="name" placeholder="E-mail" class="require">
+		<input type="text" name="name" placeholder="Телефон" class="require">
+		<textarea name="text" placeholder="Текст письма" class="require"></textarea>
+		<div class="error">Ошибка отправки. Необходимо заполнить все поля</div>
+		<input type="submit" value="Отправить">
+		<div class="sucsess">
+			<p>
+				Спасибо за ваше
+				сообщение.<br>
+				Мы рассмотрим
+				и свяжемся с вами.
+
+			</p>
+		</div>
+	</form>
+</div>
+<div id="rss" class="module-window">
+	<div class="title"><img src="../bitrix/templates/main/img/green-mail.png">
+		<p>Подписаться на рассылку</p>
+	</div>
+	<form method="post">
+		<input type="text" name="name" placeholder="Имя" class="require">
+		<input type="text" name="name" placeholder="E-mail" class="require">
+		<div class="error">Ошибка отправки. Необходимо заполнить все поля</div>
+		<input type="submit" value="Отправить">
+		<div class="sucsess">
+			<p>Спасибо.<br>
+				Вы подписаны<br>
+				на рассылку.
+			</p>
+		</div>
+	</form>
+</div>
+</body>
 </html>
--- bitrix/templates/main/header.php
+++ bitrix/templates/main/header.php
@@ -1,17 +1,50 @@
 <?
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
+$tplPath = $APPLICATION->GetTemplatePath();
+global $tplPath;
 ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<?$APPLICATION->ShowHead();?>
-		<title><?$APPLICATION->ShowTitle();?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 	
-	</head>
-	<body>
-		<div id="panel">
-			<?$APPLICATION->ShowPanel();?>
-		</div>
-	
-						
+<html lang="ru">
+  <head>
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content-width="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <?$APPLICATION->ShowHead();?>
+    <link rel="stylesheet" href="<?=$tplPath;?>/styles/build/build.css">
+    <script type="text/javascript" src="<?=$tplPath;?>/scripts/build/build.js"></script>
+  </head>
+  <body>
+		<?$APPLICATION->ShowPanel();?>
+		<header>
+      <div class="wrapper">
+        <div id="logo" class="column">
+          <div class="logo"></div>
+        </div>
+        <div id="menu" class="column">
+          <nav>
+            <ul>
+              <li><a href="#">О компании</a></li>
+              <li><a href="#">Каталог услуг</a>
+                <ul class="animated">
+                  <li><a href="#" class="active">Бухгалтерский и налоговый учет</a></li>
+                  <li><a href="#">Корпоративные юридические услуги</a></li>
+                  <li><a href="#">Кадровое делопроизводство</a></li>
+                  <li><a href="#">Аудит</a></li>
+                  <li><a href="#">Безопасность труда на производстве</a></li>
+                  <li><a href="#">Подбор персонала</a></li>
+                  <li><a href="#">Услуги для физических лиц</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Изменения в законодательстве</a></li>
+              <li><a href="#">Контакты</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div id="contacts" class="column">
+          <div class="figure"><a href="tel:+79150070775">+7 (915) 007-07-75</a><a href="#present">Презентация</a></div><span class="callme"><a href="#callme" class="call-window">Заказать звонок</a></span><span class="writeme"><a href="#writeme" class="call-window">Написать нам</a></span>
+        </div>
+      </div>
+    </header>
+    <main class="content main-page">
