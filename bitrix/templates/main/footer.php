82b47bd8657cf7c3660043765960a709243192df
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-10-29 13:29:35 +0300

    Add module addres from footer

--- bitrix/templates/main/footer.php
+++ bitrix/templates/main/footer.php
@@ -6,28 +6,84 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 <footer>
 	<div class="wrapper">
 		<div id="copy" class="column">© Biz Partners, 2001-2015
-			<p><b>Адрес: 107023 г. Москва,</b> <br>
-				ул. Большая почтовая, 36, <br>
-				18 подъезд, сектор А, 1 этаж, офис 110
-
+			<p>
+				<?$APPLICATION->IncludeComponent(
+	"bitrix:news.detail", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => ".default",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "N",
+		"DISPLAY_PICTURE" => "N",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"ELEMENT_CODE" => "",
+		"ELEMENT_ID" => "2",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"IBLOCK_ID" => "1",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_URL" => "",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Страница",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_CANONICAL_URL" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"USE_PERMISSIONS" => "N",
+		"USE_SHARE" => "N"
+	),
+	false
+);?>
 			</p>
 		</div>
 		<div id="footer-menu" class="column">
 			<?$APPLICATION->IncludeComponent("bitrix:menu", "bottom_menu", Array(
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-		"COMPONENT_TEMPLATE" => ".default",
-		"DELAY" => "N",	// Откладывать выполнение шаблона меню
-		"MAX_LEVEL" => "1",	// Уровень вложенности меню
-		"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
-		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
-		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-		"ROOT_MENU_TYPE" => "bottom",	// Тип меню для первого уровня
-		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-	),
-	false
-);?>
+				"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+					"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+					"COMPONENT_TEMPLATE" => ".default",
+					"DELAY" => "N",	// Откладывать выполнение шаблона меню
+					"MAX_LEVEL" => "1",	// Уровень вложенности меню
+					"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+					"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+					"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+					"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+					"ROOT_MENU_TYPE" => "bottom",	// Тип меню для первого уровня
+					"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+				),
+				false
+			);?>
 			<ul>
 				<li>Каталог услуг</li>
 				<li><a href="#">Бухгалтерский и налоговыйучет</a></li>
