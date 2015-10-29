e875a5a4acd2208dcc2989c7500bafe9d20969fd
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-10-29 12:51:40 +0300

    Add module from header presentation and telephone number

--- bitrix/templates/main/header.php
+++ bitrix/templates/main/header.php
@@ -28,26 +28,88 @@ global $tplPath;
 					<?}?>
 				</div>
 				<?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"main_menu", 
+					"bitrix:menu",
+					"",
+					Array(
+						"ALLOW_MULTI_SELECT" => "N",
+						"CHILD_MENU_TYPE" => "left",
+						"COMPONENT_TEMPLATE" => ".default",
+						"DELAY" => "N",
+						"MAX_LEVEL" => "1",
+						"MENU_CACHE_GET_VARS" => array(""),
+						"MENU_CACHE_TIME" => "3600",
+						"MENU_CACHE_TYPE" => "A",
+						"MENU_CACHE_USE_GROUPS" => "Y",
+						"ROOT_MENU_TYPE" => "left",
+						"USE_EXT" => "N"
+					)
+				);?>
+				<div id="contacts" class="column">
+					<?$APPLICATION->IncludeComponent(
+	"bitrix:news.detail", 
+	"header_tel_present", 
 	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "main_menu",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "header_tel_present",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"ELEMENT_CODE" => "",
+		"ELEMENT_ID" => "1",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
 		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N"
+		"IBLOCK_ID" => "1",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_URL" => "",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Страница",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "ATT_FILE",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_CANONICAL_URL" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"USE_PERMISSIONS" => "N",
+		"USE_SHARE" => "N"
 	),
 	false
 );?>
-				<div id="contacts" class="column">
-					<div class="figure"><a href="tel:+79150070775">+7 (915) 007-07-75</a><a href="#present">Презентация</a></div><span class="callme"><a href="#callme" class="call-window">Заказать звонок</a></span><span class="writeme"><a href="#writeme" class="call-window">Написать нам</a></span>
+					<span class="callme">
+						<a href="#callme" class="call-window">Заказать звонок</a>
+					</span>
+					<span class="writeme">
+						<a href="#writeme" class="call-window">Написать нам</a>
+					</span>
 				</div>
 			</div>
 		</header>
--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
-?><?$APPLICATION->IncludeComponent("bitrix:menu", "main_menu", Array(
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-		"COMPONENT_TEMPLATE" => ".default",
-		"DELAY" => "N",	// Откладывать выполнение шаблона меню
-		"MAX_LEVEL" => "1",	// Уровень вложенности меню
-		"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
-		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
-		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-		"ROOT_MENU_TYPE" => "left",	// Тип меню для первого уровня
-		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-	),
-	false
-);?><?
+?><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
