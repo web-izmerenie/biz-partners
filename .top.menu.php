3ac5bdced7c2563660b3d472e6a08e9d6e39ebc0
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2016-07-22 17:14:09 +0300

    Add logic view content without subdomen

--- .top.menu.php
+++ .top.menu.php
@@ -1,32 +1,32 @@
 <?
 $aMenuLinks = Array(
 	Array(
-		"О компании", 
-		"/o-kompanii/", 
-		Array(), 
-		Array(), 
-		"" 
+		"О компании",
+		"http://biz-partners.ru/o-kompanii/",
+		Array(),
+		Array(),
+		""
 	),
 	Array(
-		"Услуги", 
-		"/uslugi/", 
-		Array(), 
-		Array("INSILDE_MENU"=>"Y"), 
-		"" 
+		"Услуги",
+		"http://biz-partners.ru/uslugi/",
+		Array(),
+		Array("INSILDE_MENU"=>"Y"),
+		""
 	),
 	Array(
-		"Изменения в законодательстве", 
-		"/izmeneniya-v-zakonodatelstve/", 
-		Array(), 
-		Array(), 
-		"" 
+		"Изменения в законодательстве",
+		"http://biz-partners.ru/izmeneniya-v-zakonodatelstve/",
+		Array(),
+		Array(),
+		""
 	),
 	Array(
-		"Контакты", 
-		"/kontakty/", 
-		Array(), 
-		Array(), 
-		"" 
+		"Контакты",
+		"http://biz-partners.ru/kontakty/", 
+		Array(),
+		Array(),
+		""
 	)
 );
 ?>
--- index.php
+++ index.php
@@ -1,176 +1,266 @@
 <?
-define('MAIN', 'Y');
+if($_SERVER["HTTP_HOST"] != "dev.biz-partners.ru"){
+	define("CATALOG_DETAIL", "Y");
+}else{
+	define('MAIN', 'Y');
+}
 define('NO_WRAP', 'Y');
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty("title", "Biz-Partners - услуги бухгалтерского аудита");
 $APPLICATION->SetTitle("Главная");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section.list",
-	"module_mainpage_catalog",
-	Array(
-		"ADD_SECTIONS_CHAIN" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"COMPONENT_TEMPLATE" => ".default",
-		"COUNT_ELEMENTS" => "Y",
-		"IBLOCK_ID" => "6",
-		"IBLOCK_TYPE" => "content",
-		"SECTION_CODE" => "",
-		"SECTION_FIELDS" => array(0=>"",1=>"",),
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],
-		"SECTION_URL" => "",
-		"SECTION_USER_FIELDS" => array(0=>"",1=>"",),
-		"SHOW_PARENT_NAME" => "Y",
-		"TOP_DEPTH" => "2",
-		"VIEW_MODE" => "LINE"
-	)
-);?>
+?>
+<?if($_SERVER["HTTP_HOST"] != "dev.biz-partners.ru"){?>
+	<?
+		$elementId = "";
 
-<section class="main-advenurage">
-	<div class="wrapper">
-		<h1>Преимущества компании «Бизнес Партнерс»</h1>
-		<ul>
-			<li><img src="../bitrix/templates/main/img/main-adventure-1.png">
-				<div class="text">Строгое соблюдение всех сроков: от текущих вопросов до отчетности.</div>
-			</li>
-			<li><img src="../bitrix/templates/main/img/main-adventure-2.png">
-				<div class="text">Финансовая ответственность за предложенные решения.</div>
-			</li>
-			<li><img src="../bitrix/templates/main/img/main-adventure-3.png">
-				<div class="text">
-					Профессионализм и опытность сотрудников – более 10 лет опыта
-					<br> работы.
-				</div>
-			</li>
-			<li><img src="../bitrix/templates/main/img/main-adventure-4.png">
-				<div class="text">Вы платите только за необходимые вам услуги.</div>
-			</li>
-		</ul>
-	</div>
-</section>
-<section class="main-news">
-	<div class="wrapper">
-		<div class="column">
-			<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"module_news_main",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => ".default",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => "",
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "3",
-		"IBLOCK_TYPE" => "content",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "1",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => "",
-		"SET_BROWSER_TITLE" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
-);?>
+		if($_SERVER["HTTP_HOST"] == "bankrotstvo-ooo.biz-partners.ru"){
+			$elementId = 268;
+		}else if($_SERVER["HTTP_HOST"] == "razreshinie-na-rabotu.biz-partners.ru"){
+			$elementId = 245;
+		}
+	?>
+	<?$APPLICATION->IncludeComponent(
+		"bitrix:catalog.element",
+		"subitem_detail",
+		Array(
+			"ACTION_VARIABLE" => "action",
+			"ADD_DETAIL_TO_SLIDER" => "N",
+			"ADD_ELEMENT_CHAIN" => "N",
+			"ADD_PROPERTIES_TO_BASKET" => "Y",
+			"ADD_SECTIONS_CHAIN" => "Y",
+			"BACKGROUND_IMAGE" => "-",
+			"BASKET_URL" => "/personal/basket.php",
+			"BRAND_USE" => "N",
+			"BROWSER_TITLE" => "NAME",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_SECTION_ID_VARIABLE" => "N",
+			"DETAIL_PICTURE_MODE" => "IMG",
+			"DETAIL_URL" => "",
+			"DISABLE_INIT_JS_IN_COMPONENT" => "N",
+			"DISPLAY_COMPARE" => "N",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PREVIEW_TEXT_MODE" => "E",
+			"ELEMENT_CODE" => "",
+			"ELEMENT_ID" => $elementId,
+			"IBLOCK_ID" => "14",
+			"IBLOCK_TYPE" => "catalog",
+			"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+			"LINK_IBLOCK_ID" => "",
+			"LINK_IBLOCK_TYPE" => "",
+			"LINK_PROPERTY_SID" => "",
+			"MESSAGE_404" => "",
+			"MESS_BTN_ADD_TO_BASKET" => "В корзину",
+			"MESS_BTN_BUY" => "Купить",
+			"MESS_BTN_COMPARE" => "Сравнить",
+			"MESS_BTN_SUBSCRIBE" => "Подписаться",
+			"MESS_NOT_AVAILABLE" => "Нет в наличии",
+			"META_DESCRIPTION" => "-",
+			"META_KEYWORDS" => "-",
+			"OFFERS_LIMIT" => "0",
+			"PARTIAL_PRODUCT_PROPERTIES" => "N",
+			"PRICE_CODE" => array(),
+			"PRICE_VAT_INCLUDE" => "Y",
+			"PRICE_VAT_SHOW_VALUE" => "N",
+			"PRODUCT_ID_VARIABLE" => "id",
+			"PRODUCT_PROPERTIES" => array(),
+			"PRODUCT_PROPS_VARIABLE" => "prop",
+			"PRODUCT_QUANTITY_VARIABLE" => "",
+			"PROPERTY_CODE" => array("ATT_MORE_SERVICE", "ATT_DANGER_TEXT", "ATT_TEXT", "ATT_TEXT_2", "ATT_TEL", "ATT_TEMPLATE", ""),
+			"SECTION_CODE" => "",
+			"SECTION_ID" => $_REQUEST["SECTION_ID"],
+			"SECTION_ID_VARIABLE" => "SECTION_ID",
+			"SECTION_URL" => "",
+			"SEF_MODE" => "N",
+			"SET_BROWSER_TITLE" => "Y",
+			"SET_CANONICAL_URL" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "Y",
+			"SHOW_404" => "N",
+			"SHOW_DEACTIVATED" => "N",
+			"SHOW_PRICE_COUNT" => "1",
+			"TEMPLATE_THEME" => "blue",
+			"USE_COMMENTS" => "N",
+			"USE_ELEMENT_COUNTER" => "Y",
+			"USE_MAIN_ELEMENT_SECTION" => "N",
+			"USE_PRICE_COUNT" => "N",
+			"USE_PRODUCT_QUANTITY" => "N",
+			"USE_VOTE_RATING" => "N"
+		)
+	);?>
+<?}else{?>
+	<?$APPLICATION->IncludeComponent(
+		"bitrix:catalog.section.list",
+		"module_mainpage_catalog",
+		Array(
+			"ADD_SECTIONS_CHAIN" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"COMPONENT_TEMPLATE" => ".default",
+			"COUNT_ELEMENTS" => "Y",
+			"IBLOCK_ID" => "6",
+			"IBLOCK_TYPE" => "content",
+			"SECTION_CODE" => "",
+			"SECTION_FIELDS" => array(0=>"",1=>"",),
+			"SECTION_ID" => $_REQUEST["SECTION_ID"],
+			"SECTION_URL" => "",
+			"SECTION_USER_FIELDS" => array(0=>"",1=>"",),
+			"SHOW_PARENT_NAME" => "Y",
+			"TOP_DEPTH" => "2",
+			"VIEW_MODE" => "LINE"
+		)
+	);?>
+
+	<section class="main-advenurage">
+		<div class="wrapper">
+			<h1>Преимущества компании «Бизнес Партнерс»</h1>
+			<ul>
+				<li><img src="../bitrix/templates/main/img/main-adventure-1.png">
+					<div class="text">Строгое соблюдение всех сроков: от текущих вопросов до отчетности.</div>
+				</li>
+				<li><img src="../bitrix/templates/main/img/main-adventure-2.png">
+					<div class="text">Финансовая ответственность за предложенные решения.</div>
+				</li>
+				<li><img src="../bitrix/templates/main/img/main-adventure-3.png">
+					<div class="text">
+						Профессионализм и опытность сотрудников – более 10 лет опыта
+						<br> работы.
+					</div>
+				</li>
+				<li><img src="../bitrix/templates/main/img/main-adventure-4.png">
+					<div class="text">Вы платите только за необходимые вам услуги.</div>
+				</li>
+			</ul>
 		</div>
-		<div class="column">
-			<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"module_news_main",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "module_news_main",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(0=>"",1=>"",),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "4",
-		"IBLOCK_TYPE" => "content",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "1",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(0=>"",1=>"",),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
-);?>
+	</section>
+	<section class="main-news">
+		<div class="wrapper">
+			<div class="column">
+				<?$APPLICATION->IncludeComponent(
+		"bitrix:news.list",
+		"module_news_main",
+		Array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"COMPONENT_TEMPLATE" => ".default",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => "",
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"IBLOCK_ID" => "3",
+			"IBLOCK_TYPE" => "content",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "1",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => "",
+			"SET_BROWSER_TITLE" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "N",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC"
+		)
+	);?>
+			</div>
+			<div class="column">
+				<?$APPLICATION->IncludeComponent(
+		"bitrix:news.list",
+		"module_news_main",
+		Array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"COMPONENT_TEMPLATE" => "module_news_main",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => array(0=>"",1=>"",),
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"IBLOCK_ID" => "4",
+			"IBLOCK_TYPE" => "content",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "1",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => array(0=>"",1=>"",),
+			"SET_BROWSER_TITLE" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "N",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC"
+		)
+	);?>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+<?}?>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
